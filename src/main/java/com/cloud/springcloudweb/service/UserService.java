ee0343292770707bc7f7c3ae279e5bf7875d99e1
glenn <glenn.jo@kakaocorp.com>
2019-12-18 14:41:30 +0900

    ing...

---
+++ src/main/java/com/cloud/springcloudweb/service/UserService.java
@@ -0,0 +1,9 @@
+package com.cloud.springcloudweb.service;
+
+import com.cloud.springcloudweb.model.User;
+import com.cloud.springcloudweb.service.common.SearchServiceAbstract;
+
+public class UserService extends SearchServiceAbstract<User> {
+
+
+}
